eb8763f4592e1196ecf5d571ddd613e693c26b92
Davi Freire <fsdavi.dev@gmail.com>
2023-06-18 17:48:36 -0300

    Create jest test for isBase64 method

---
+++ src/tests/isBase64.test.js
@@ -0,0 +1,13 @@
+import isBase64 from '../lib/isBase64';
+
+describe('isBase64', () => {
+ test('Verificar se uma string é uma representação válida em Base64', () => {
+   expect(isBase64('SGVsbG8gd29ybGQh')).toBe(true);
+   expect(isBase64('VGhpcyBpcyBhIGJhc2U2NCBlbmNvZGluZyBzdHJpbmc=')).toBe(true);
+ });
+
+ test('Verificar se uma string não é uma representação válida em Base64', () => {
+   expect(isBase64('Qualquer coisa')).toBe(false);
+   expect(isBase64('Não é Base64')).toBe(false);
+ });
+});
