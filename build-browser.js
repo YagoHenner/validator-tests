6b8cd6e22def959e3a16c8c2d9438253dd92c969
Davi Freire <fsdavi.dev@gmail.com>
2023-06-21 00:14:52 -0300

    Cria e ajusta configurações para o jest

--- build-browser.js
+++ build-browser.js
@@ -1,12 +1,12 @@
 /* eslint import/no-extraneous-dependencies: 0 */
-import fs from "fs";
-import { rollup } from "rollup";
-import babel from "rollup-plugin-babel";
-import babelPresetEnv from "@babel/preset-env";
-import pkg from "./package.json";
+import fs from 'fs';
+import { rollup } from 'rollup';
+import babel from 'rollup-plugin-babel';
+import babelPresetEnv from '@babel/preset-env';
+import pkg from './package.json';
 
 rollup({
-  entry: "src/index.js",
+  entry: 'src/index.js',
   plugins: [
     babel({
       presets: [[babelPresetEnv, { modules: false }]],
@@ -14,18 +14,17 @@ rollup({
     }),
   ],
 })
-  .then((bundle) =>
+  .then(bundle =>
     bundle.write({
-      dest: "validator.js",
-      format: "umd",
+      dest: 'validator.js',
+      format: 'umd',
       moduleName: pkg.name,
-      banner: `/*!\n${String(fs.readFileSync("./LICENSE"))
+      banner: `/*!\n${String(fs.readFileSync('./LICENSE'))
         .trim()
-        .split("\n")
-        .map((l) => ` * ${l}`)
-        .join("\n")}\n */`,
-    })
-  )
+        .split('\n')
+        .map(l => ` * ${l}`)
+        .join('\n')}\n */`,
+    }))
   .catch((e) => {
     process.stderr.write(`${e.message}\n`);
     process.exit(1);
