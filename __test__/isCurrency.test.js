449a43c4df3c34f1d5f2849b4aa61deb07a8f4cc
Davi Freire <fsdavi.dev@gmail.com>
2023-07-12 17:10:23 -0300

    Melhora os testes do método isCurrency

--- __test__/isCurrency.test.js
+++ __test__/isCurrency.test.js
@@ -1,37 +1,117 @@
 import isCurrency from '../src/lib/isCurrency';
+import assertString from '../src/lib/util/assertString';
+import merge from '../src/lib/util/merge';
+
+jest.mock('./util/assertString', () => ({
+  __esModule: true,
+  default: jest.fn(),
+}));
+
+jest.mock('./util/merge', () => ({
+  __esModule: true,
+  default: jest.fn((options, defaultOptions) => ({
+    ...defaultOptions,
+    ...options,
+  })),
+}));
+
+const defaultOptions = {
+  symbol: '$',
+  require_symbol: false,
+  allow_space_after_symbol: false,
+  symbol_after_digits: false,
+  allow_negatives: true,
+  parens_for_negatives: false,
+  negative_sign_before_digits: false,
+  negative_sign_after_digits: false,
+  allow_negative_sign_placeholder: false,
+  thousands_separator: ',',
+  decimal_separator: '.',
+  allow_decimal: true,
+  require_decimal: false,
+  digits_after_decimal: [2],
+  allow_space_after_digits: false,
+};
 
 describe('isCurrency', () => {
-  test('Verificar se a string é uma moeda válida (retorna true)', () => {
-    expect(isCurrency('$100')).toBe(true);
-    expect(isCurrency('$1,000,000.50')).toBe(true);
-    expect(isCurrency('-$50.00')).toBe(true);
-    expect(isCurrency('R$123', { symbol: 'R$' })).toBe(true);
-    expect(isCurrency('€10', { symbol: '€' })).toBe(true);
+  beforeEach(() => {
+    assertString.mockClear();
+    merge.mockClear();
   });
 
-  test('Verificar se a string não é uma moeda válida (retorna false)', () => {
-    expect(isCurrency('aaa')).toBe(false);
-    expect(isCurrency('USD 100')).toBe(false);
-    expect(isCurrency('$10,00')).toBe(false);
-    expect(isCurrency('-$50.0')).toBe(false);
-    expect(isCurrency('R123')).toBe(false);
-    expect(isCurrency('€10,00')).toBe(false);
-    expect(isCurrency('')).toBe(false);
+  test('Deve retornar true para currency válida', () => {
+    const string = '$1,000.00';
+    const options = {};
+
+    const result = isCurrency(string, options);
+
+    expect(result).toBe(true);
+    expect(assertString).toHaveBeenCalledWith(string);
+    expect(merge).toHaveBeenCalledWith(options, defaultOptions);
   });
 
-  test('Verificar se está utilizando as opções default', () => {
-    expect(isCurrency('$100')).toBe(true);
-    expect(isCurrency('$100', {})).toBe(true);
+  test('Deve retornar false para string currency errada', () => {
+    const string = '12345';
+    const options = {};
+
+    const result = isCurrency(string, options);
+
+    expect(result).toBe(false);
+    expect(assertString).toHaveBeenCalledWith(string);
+    expect(merge).toHaveBeenCalledWith(options, defaultOptions);
   });
 
-  test('Verificar se as opções enviadas estão sendo utilizadas', () => {
+  test('Deve retornar true para currency válida com opções personalizadas', () => {
+    const string = '€1.000,00';
     const options = {
       symbol: '€',
-      allow_decimal: false,
+      thousands_separator: '.',
+      decimal_separator: ',',
+      digits_after_decimal: [2],
     };
 
-    expect(isCurrency('$100')).toBe(true);
-    expect(isCurrency('$100', options)).toBe(false);
-    expect(isCurrency('€100', options)).toBe(true);
+    const result = isCurrency(string, options);
+
+    expect(result).toBe(true);
+    expect(assertString).toHaveBeenCalledWith(string);
+    expect(merge).toHaveBeenCalledWith(options, defaultOptions);
+  });
+
+  test('Deve retornar true para currency negativa válida com opções personalizadas', () => {
+    const string = '-$1,000.00';
+    const options = {
+      symbol: '$',
+      allow_negatives: true,
+    };
+
+    const result = isCurrency(string, options);
+
+    expect(result).toBe(true);
+    expect(assertString).toHaveBeenCalledWith(string);
+    expect(merge).toHaveBeenCalledWith(options, defaultOptions);
+  });
+
+  test('Deve retornar false para currency com dígitos decimais ausentes ', () => {
+    const string = '$1,000.';
+    const options = {};
+
+    const result = isCurrency(string, options);
+
+    expect(result).toBe(false);
+    expect(assertString).toHaveBeenCalledWith(string);
+    expect(merge).toHaveBeenCalledWith(options, defaultOptions);
+  });
+
+  test('Deve retornar falso para currency com separador de milhares inválido', () => {
+    const string = '$1.000,00';
+    const options = {
+      thousands_separator: ',',
+    };
+
+    const result = isCurrency(string, options);
+
+    expect(result).toBe(false);
+    expect(assertString).toHaveBeenCalledWith(string);
+    expect(merge).toHaveBeenCalledWith(options, defaultOptions);
   });
 });
